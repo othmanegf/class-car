68f40dc73c9925e6c56cd433c74823fc2164f4eb
othmane <147095181+othmanegf@users.noreply.github.com>
2023-11-20 21:58:47 +0100

    Create CAR.java

---
+++ CAR.java
@@ -0,0 +1,18 @@
+class CAR {
+    short speed;
+    short enginePower;
+    String color;
+    String name;
+
+
+
+    public void start() {
+        System.out.println("The car is starting.");
+    }
+    public void stop() {
+        System.out.println("The car is stopping.");
+    }
+    public void changeGear() {
+        System.out.println("Changing gears.");
+    }
+}
